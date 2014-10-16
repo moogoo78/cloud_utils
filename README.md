29a407214ccbc702544afca4d1b4a41cd1573233
MooGoo <moogoo78@gmail.com>
2014-10-16 16:07:23 +0800

    first impont

---
+++ README.md
@@ -0,0 +1,10 @@
+# Readme
+
+## Vagrant
+
+vagrant up eins
+
+
+## startup.sh
+
+now for centos 6.5
---
+++ startup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+function te()
+{
+    ls -alh
+}
+
+
+te
+
+echo -e "abc\nvvv\n$HOME" >> aaa
